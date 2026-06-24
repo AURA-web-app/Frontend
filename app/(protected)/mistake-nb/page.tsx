6e22b03277874f7e0805df1f8ba7e1916d3a84b0
Rudra <rudra070311@gmail.con>
2026-06-24 21:49:25 +0530

    feat: pages completed. timer done, login done, backed required next

--- app/(protected)/mistake-nb/page.tsx
+++ app/(protected)/mistake-nb/page.tsx
@@ -1,7 +1,7 @@
-export default function Page() {
+export default function MistakeNB() {
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <h1 className="text-3xl font-bold">Dashboard</h1>
+      <h1 className="text-3xl font-bold">Mistake NB</h1>
     </div>
   )
 }
--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect, useRef } from "react";
 import "../style/auth.css";
 import "../style/theme.css";
