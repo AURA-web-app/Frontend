7732230eefcd8eb0ddc0ddfd3d578c7dc67059b5
Rudra <rudra070311@gmail.con>
2026-06-29 13:01:27 +0530

    fix: imports fixed in layout.tsx

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import "./style/theme.css";
+import "../globals.css";
+import "../style/theme.css";
 import Navbar from "../../components/navbar"
 
 const geistSans = Geist({
